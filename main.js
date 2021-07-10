6f59cd495cf50546c9935af474b18ddba2da8174
LeeWoojin-99 <lwj1316@naver.com>
2021-07-10 16:59:29 +0900

    javascript -> jquery 변환 완료

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 function right_menu_panel_on(){
-    var target = document.getElementById("right_menu_panel");
-    target.style.transform = "translateX(-700px)";
-    console.log("right_menu_panel_on()")
+    // var target = document.getElementById("right_menu_panel");
+    // target.style.transform = "translateX(-700px)";
+    var target = $('#right_menu_panel');
+    target.css('transform', 'translateX(-700px)');
 }
 
 function right_menu_panel_off(){
-    var target = document.getElementById("right_menu_panel");
-    target.style.transform = "translateX(700px)";
+    // var target = document.getElementById("right_menu_panel");
+    // target.style.transform = "translateX(700px)";
+    var target = $('#right_menu_panel');
+    target.css('transform', 'translateX(700px)');
 }
 
 function product_search_on(){
-    var target = document.getElementById("product_search_wrapper");
-    target.style.display = "block";
-    target.style.animation = "fade_in 0.6s";
+    // var target = document.getElementById("product_search_wrapper");
+    // target.style.opacity = "1";
+    // target.style.visibility = 'visible';
+    var target = $('#product_search_wrapper');
+    target.css('opacity', '1').css('visibility', 'visible');
 }
 
 function product_search_off(){
-    var target = document.getElementById("product_search_wrapper");
-    setTimeout(() => target.style.display = "none", 600)
-    target.style.animation = "fade_out 0.6s";
+    // var target = document.getElementById("product_search_wrapper");
+    // target.style.opacity = "0";
+    // target.style.visibility = 'hidden';
+    var target = $('#product_search_wrapper');
+    target.css('opacity', '0').css('visibility', 'hidden');
 }
